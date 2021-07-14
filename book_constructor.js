b2bf6893a840428343a360c4b3cd592d4e58eed6
Luke <kingluke2222@live.com>
2021-07-14 14:50:03 -0700

    add form and new book function

--- book_constructor.js
+++ book_constructor.js
@@ -1,3 +1,5 @@
+let ourBooks = [];
+
 function book(title, author, pages, is_read)
 {
     this.title = title
@@ -6,10 +8,10 @@ function book(title, author, pages, is_read)
     this.read = false
     if (is_read == true)
     {
-        this.read = 'read'
+        this.read = 'true'
     }else
     {
-        this.read = "not read yet"
+        this.read = "false"
     }
 
     this.info = function(){
@@ -18,5 +20,25 @@ function book(title, author, pages, is_read)
 }
 
 
-let hobbit = new book("The Hobbit", "J.R.R. Tolkien", "295 pages", false)
-console.log(hobbit.info())
+function addBookToLibaray(){
+    var title = document.forms['book-form']['title'].value;
+    var author = document.forms['book-form']['author'].value;
+    var pageCount = document.forms['book-form']['page-count'].value;
+    var read = document.forms['book-form']['is-read'].value;
+    
+    var newBook = new book(title, author, pageCount, read);
+    ourBooks.push(newBook);
+    var bookList = document.getElementsByClassName('book-list')[0];
+    var div = document.createElement('div');
+
+    var p = document.createElement('p');
+    p.innerHTML = newBook.info();
+    console.log(newBook.info())
+    div.append(p);
+
+    bookList.append(div)
+}
+
+
+
+
