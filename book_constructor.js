0cd489fe3df2ca201b96d556a539a774a1834f25
Luke <kingluke2222@live.com>
2021-07-17 14:26:55 -0700

    add read status toggle button

--- book_constructor.js
+++ book_constructor.js
@@ -1,27 +1,28 @@
 let ourBooks = [];
-var addButton = document.querySelector('.book-submit')
-addButton.addEventListener('click', (e) => {
+var submitButton = document.querySelector('.book-submit')
+submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     addBookToLibaray()});
 
 var addButton = document.querySelector('.add-book')
 addButton.addEventListener('click', () => {
-    toggleBookForm()});
+    toggleBookForm(addButton)});
+    
 
 function book(title, author, pages, is_read)
 {
     this.title = title
     this.author = author
     this.pages = pages
-    this.read = false
-    if (is_read == true)
+    this.read = 'read'
+    if (is_read == 'true')
     {
         this.read = 'read'
+        
     }else
     {
         this.read = "Not read yet"
     }
-
     this.info = function(){
         return `${title}, ${author}, ${pages} pages, ${this.read}`
     }
@@ -33,7 +34,6 @@ function addBookToLibaray(){
     let author = document.forms['book-form']['author'].value;
     let pageCount = document.forms['book-form']['page-count'].value;
     let read = document.forms['book-form']['is-read'].value;
-    
     let newBook = new book(title, author, pageCount, read);
     ourBooks.push(newBook);
     var bookIndex = ourBooks.length - 1;
@@ -47,12 +47,19 @@ function addBookToLibaray(){
     let closeBtn = document.createElement('button');
     closeBtn.classList.add('remove-btn');
     closeBtn.textContent = 'Remove';
+
+    let toggleReadbtn = document.createElement('button');
+    toggleReadbtn.classList.add('read-btn');
+    toggleReadbtn.textContent = 'Change Read Status';
     
 
+    div.appendChild(toggleReadbtn);
     div.appendChild(closeBtn);
+    
 
     div.setAttribute('book-index', bookIndex);
 
+    toggleReadbtn.addEventListener('click', () => {toggleBookRead(ourBooks[bookIndex], div)});
     closeBtn.addEventListener('click', () => {removeBook(bookIndex, div)});
 
     bookList.appendChild(div);
@@ -65,12 +72,14 @@ function createBookCard(div, info){
 
 }
 
-function toggleBookForm(){
+function toggleBookForm(addButton){
     let form = document.querySelector('.form-toggle');
     if (form.style.display === "none")
     {
+        addButton.textContent = 'Close';
         form.style.display = 'block';
     } else {
+        addButton.textContent = 'New Book';
         form.style.display = 'none';
     }
 }
@@ -78,6 +87,21 @@ function toggleBookForm(){
 function removeBook(bookIndex, div){
     ourBooks.splice(bookIndex, bookIndex);
     div.remove();
+}
+
+function toggleBookRead(thisBook, div){
+    if (thisBook.read == 'read')
+    {
+        thisBook.read = 'Not read yet';
+        div.children[3].textContent = thisBook.read;
+        
+    }else
+    {
+        thisBook.read = "read";
+        div.children[3].textContent = thisBook.read;
+    }
+
+    
 }
 
 
